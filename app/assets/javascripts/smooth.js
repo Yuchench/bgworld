7b4d085c3b9224a3243a04f47e9c5fb64a1cc7ea
yuchen <ycchang1006@gmail.com>
2014-05-28 12:09:59 +0800

    layout adjustments

--- app/assets/javascripts/smooth.js
+++ app/assets/javascripts/smooth.js
@@ -1,12 +1,31 @@
+// $(function() {
+//   $('a[href*=#]:not([href=#])').click(function() {
+//     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+//       var target = $(this.hash);
+//       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+//       if (target.length) {
+//         $('html,body').animate({
+//           scrollTop: target.offset().top
+//         }, 800);
+//         return false;
+//       }
+//     }
+//   });
+// });
+
 $(function() {
   $('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
+        var currentOffset = $(document).scrollTop();
+        var targetOffset = target.offset().top;
+        var distance = Math.abs(currentOffset-targetOffset);
+        console.log(distance);
         $('html,body').animate({
           scrollTop: target.offset().top
-        }, 1000);
+        }, distance/2000*1000);
         return false;
       }
     }
--- app/assets/javascripts/sticky.js
+++ app/assets/javascripts/sticky.js
@@ -5,9 +5,13 @@ var stickyNav = function(){
 var scrollTop = $(window).scrollTop();  
        
 if (scrollTop > stickyNavTop) {   
-    $('.navbar').addClass('sticky');  
+    $('.navbar').addClass('sticky'); 
+    $('body').addClass('stickyBody'); 
+    $('#aboutspacer').css('height','30px');
 } else {  
-    $('.navbar').removeClass('sticky');   
+    $('.navbar').removeClass('sticky');  
+    $('body').removeClass('stickyBody'); 
+    $('#aboutspacer').css('height','0px');
 }  
 };
   
@@ -17,3 +21,21 @@ $(window).scroll(function() {
     stickyNav();  
 });  
 });
+
+// jQuery(function($) {
+//   $(document).ready( function() {
+//     //enabling stickUp on the '.navbar-wrapper' class
+//     $('.navbar').stickUp({
+//                   parts: {
+//                     0: 'about',
+//                     1: 'new',
+//                     2: 'menu',
+//                     3: 'board',
+//                     4: 'activity',
+//                     5: 'contact',
+//                   },
+//                   itemClass: 'about',
+//                   itemHover: 'active'
+//                 });
+//   });
+// });
